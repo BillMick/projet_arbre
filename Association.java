18211ed327c2889668a277df5ff6e09808593a7e
Michaël A. <ahouandjinoubill@gmail.com>
2025-01-15 22:28:07 +0100

    méthode demandeDeDon: ébauche.

--- Association.java
+++ Association.java
@@ -9,5 +9,14 @@ public class Association {
     private ArrayList<Arbre> classification = new ArrayList<Arbre>;
     private ArrayList<Activite> activites = new ArrayList<Activite>;
     private ArrayList<Rapport> rapports = new ArrayList<Rapport>;
+    private ArrayList<Recette> dons = new ArrayList<Recette>;
+    private ArrayList<Recette> cotisations = new ArrayList<Recette>;
+
+    public boolean demandeDeDon(int montant, String donateur) {
+        Recette don = ... // Création de l'objet Recette
+        dons.add(don); // Ajout à la liste des dons
+        this.recette(montant); // Ajout au budget
+        // Persistance de données
+    }
 
 }
