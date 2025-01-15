ec12c8221602c952d4960207b25132f6628c959d
Michaël A. <ahouandjinoubill@gmail.com>
2025-01-15 22:17:50 +0100

    attributs de la classe Association: ébauche.

---
+++ Association.java
@@ -0,0 +1,13 @@
+public class Association {
+
+    private String nom;
+    private int budget;
+    private Membre president;
+    private ArrayList<Depense> = new ArrayList<Depense>;
+    private ArrayList<Membre> membres = new ArrayList<Membre>;
+    private ArrayList<int> annees_exercice= new ArrayList<int>;
+    private ArrayList<Arbre> classification = new ArrayList<Arbre>;
+    private ArrayList<Activite> activites = new ArrayList<Activite>;
+    private ArrayList<Rapport> rapports = new ArrayList<Rapport>
+
+}
