df077f9f87256ebfae0f2b328fb7ef3e5192a699
Michaël A. <ahouandjinoubill@gmail.com>
2025-01-15 23:14:09 +0100

    méthode sAbonner: ébauche.

--- Association.java
+++ Association.java
@@ -47,7 +47,7 @@ public class Association {
     }
 
     // s'abonner aux notifications du service des espaces verts
-    public boolean abonner() {
+    public boolean sAbonner() {
         // Persistance de données
         ajoutAbonne(this.nom); // ajout à la liste des abonnés (fonction de la classe unique ServiceDesEspacesVerts)
         return true;
