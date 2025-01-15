bc8a0edd263064bea82ecb0ab9620d4bd15bcd2d
Michaël A. <ahouandjinoubill@gmail.com>
2025-01-15 22:32:14 +0100

    méthode recette: ébauche.

--- Association.java
+++ Association.java
@@ -1,7 +1,7 @@
 public class Association {
 
     private String nom;
-    private int budget;
+    private int solde;
     private Membre president;
     private ArrayList<Depense> = new ArrayList<Depense>;
     private ArrayList<Membre> membres = new ArrayList<Membre>;
@@ -12,10 +12,18 @@ public class Association {
     private ArrayList<Recette> dons = new ArrayList<Recette>;
     private ArrayList<Recette> cotisations = new ArrayList<Recette>;
 
+    // modifier le solde
+    public int recette(int montant) {
+        this.solde += montant;
+        return this.solde;
+        // Persistance de données
+    }
+
+    // faire une demande de don / subvention
     public void demandeDeDon(int montant, String donateur) {
         Recette don = ... // Création de l'objet Recette
         dons.add(don); // Ajout à la liste des dons
-        this.recette(montant); // Ajout au budget
+        this.recette(montant); // Ajout au solde
         // Persistance de données
     }
 
